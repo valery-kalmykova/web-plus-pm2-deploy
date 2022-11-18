e6c286d60e0e0e70f8d24e4863bedb6506c6177d
valery-ish <valery-ish@yandex.ru>
2022-11-18 08:06:14 +0300

    test

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/valery-kalmykova/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy': `cd && cd /home/valery-kalmykova && rm -rf mesto-frontend`,
+      // 'pre-setup': `cd && cd /home/valery-kalmykova && rm -rf mesto-frontend`,
       'post-deploy': 'cd && cd /home/valery-kalmykova/mesto-frontend/source/frontend && npm i && npm run build',
     },
   },
