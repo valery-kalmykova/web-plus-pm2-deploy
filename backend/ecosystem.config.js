1fb2e50aa03b9986a3fc4dadc21a6a23a63736b1
valery-ish <valery-ish@yandex.ru>
2022-11-19 06:28:04 +0300

    last

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
   apps: [{
     name: 'mesto-backend',
     script: './dist/app.js',
+    cron_restart: '0 0 * * *',
   }],
 
   deploy: {
@@ -17,8 +18,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/valery-kalmykova/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      // 'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': `cd && cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 restart app && pm2 save`,
+      'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': `cd && cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 start dist/app.js --cron-restart="0 0 * * *"  && pm2 save`,
     },
   },
 };
